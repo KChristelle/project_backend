ff8e4b220b4dbe093088328063c6b353f4cba69a
KChristelle <r.ayateke@alustudent.com>
2020-04-16 15:41:50 +0200

    Implemented some oop

--- login.php
+++ login.php
@@ -20,7 +20,7 @@
                 </div>
                 <div class="form-group">
                     <label for="exampleInputPassword1">Password</label>
-                    <input type="password" name="password" class="form-control" id="exampleInputPassword1" required>
+                    <input type="password" name="pass" class="form-control" id="exampleInputPassword1" required>
                 </div>
                 <button type="submit" class="btn btn-primary">Log in</button>
             </form>
--- test.php
+++ test.php
@@ -1,25 +1,25 @@
 <?php
+    include('includes/connect_to_db.php');
+   
+
     $admin_full_name = $_POST['admin_full_name'];
     $username = $_POST['username'];
     $email =  $_POST['email'];
     $password = $_POST['password'];
 
     // DB Connection
-    $conn = new mysqli('localhost', 'root', '', 'registration');
-    if($conn -> connect_error){
-        die('Connection Failed : '. $conn-> connect_error);
+    $conn =  connectToDb();
 
-    }else{
-        $stmt = $conn->prepare("insert into admin(admin_full_name, username, email, user_password)
-        values(?, ?, ?, ?)");
-        $stmt->bind_param('sssi',$admin_full_name, $username, $email, $password);
-        $stmt->execute();
-        if ($stmt->affected_rows != 0) {
-            echo "Registration was successful";
-         } else {
-             echo 'Error: ' . $stmt->error;
-         }
-        $stmt->close();
-        $conn->close();
-    }
+    $stmt = $conn->prepare("insert into admin(admin_full_name, username, email, user_password)
+    values(?, ?, ?, ?)");
+    $stmt->bind_param('sssi',$admin_full_name, $username, $email, $password);
+    $stmt->execute();
+    if ($stmt->affected_rows != 0) {
+        echo "Registration was successful";
+        } else {
+            echo 'Error: ' . $stmt->error;
+        }
+    $stmt->close();
+    $conn->close();
+  
 ?>
